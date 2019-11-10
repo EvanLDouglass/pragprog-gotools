195939f0de777c28bf609228d995ad4264e42f16
Evan <EvanLDouglass@gmail.com>
2019-11-09 23:49:49 -0800

    Update tool to take flag args

--- interacting/todo/cmd/todo/main.go
+++ interacting/todo/cmd/todo/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"strings"
 
 	"pragprog.com/gocmd/interacting/todo"
 )
@@ -12,6 +12,23 @@ import (
 const todoFileName = ".todo.json"
 
 func main() {
+	// Customize help message
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"%s tool. Developed for The Pragmatic Bookshelp\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2020\n")
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"Usage information (only supports one option per call):\n")
+		flag.PrintDefaults()
+	}
+
+	// Get cmd line flags
+	task := flag.String("task", "", "Task to be included in the todo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to mark as complete")
+	flag.Parse()
+
+	// Generate a list to use
 	l := &todo.List{}
 
 	// Use Get to check if there are current items
@@ -20,22 +37,37 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Check number of args for todo behavior
+	// Check flags for todo behavior
 	switch {
-	// No extra args: list current items
-	case len(os.Args) == 1:
-		for _, item := range *l {
-			fmt.Println(item.Task)
+	case *complete > 0:
+		// Mark the item as complete, if it exists
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
 		}
-	// Given args: concat and use as name for a new task
-	default:
-		// Concat with space
-		item := strings.Join(os.Args[1:], " ")
-		// Add and save
-		l.Add(item)
+		// Save the new list
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+	case *task != "":
+		// Add a new task to the list
+		l.Add(*task)
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *list:
+		// List the items not yet completed
+		for _, item := range *l {
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
+		}
+	default:
+		// Invalid flag
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
 	}
 }
--- interacting/todo/cmd/todo/main_test.go
+++ interacting/todo/cmd/todo/main_test.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"os/exec"
 	"path"
-	"strings"
 	"testing"
 )
 
@@ -43,7 +42,7 @@ func TestTodoCLI(t *testing.T) {
 	cmdPath := path.Join(dir, binName)
 
 	t.Run("AddNewTask", func(t *testing.T) {
-		cmd := exec.Command(cmdPath, strings.Split(task, " ")...) // unpack str arr as args
+		cmd := exec.Command(cmdPath, "-task", task)
 
 		if err := cmd.Run(); err != nil {
 			t.Fatal(err)
@@ -51,7 +50,7 @@ func TestTodoCLI(t *testing.T) {
 	})
 
 	t.Run("ListTasks", func(t *testing.T) {
-		cmd := exec.Command(cmdPath)
+		cmd := exec.Command(cmdPath, "-list")
 		out, err := cmd.CombinedOutput()
 		if err != nil {
 			t.Fatal(err)
